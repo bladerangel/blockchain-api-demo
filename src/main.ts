f9d29fa4e22c096de1c167ae5c24300305c9000a
bladerangel <bladerangel10@gmail.com>
2019-11-11 15:35:48 -0300

    Adding global validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { BlockchainModule } from './blockchain.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(BlockchainModule);
-  await app.listen(3000);
+  const blockchainApplication = await NestFactory.create(BlockchainModule);
+  blockchainApplication.useGlobalPipes(new ValidationPipe());
+  await blockchainApplication.listen(3000);
 }
 bootstrap();
